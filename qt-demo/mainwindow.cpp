3d7cfb5f9d4b47b9eb385f8fa29810e6d15d32c0
François Desrichard <git@desrichard.fr>
2019-01-10 15:53:44 +0100

    Commenting

--- qt-demo/mainwindow.cpp
+++ qt-demo/mainwindow.cpp
@@ -16,34 +16,39 @@ namespace store {
 MainWindow::MainWindow() {
     setWindowTitle(tr("Store demo"));
 
-    using namespace std::placeholders;
 
-    // StoreInteger
+    // Store an integer and using it with two spin boxes
     auto* spin_box_1 = new QSpinBox;
-    // Subscribing via bound member function
+    // Subscribe to changes via bound member function
     MyStore::instance().integer.subscribe(
-        std::bind(&QSpinBox::setValue, spin_box_1, _1));
+        std::bind(&QSpinBox::setValue, spin_box_1, std::placeholders::_1));
+    // Set a new value when the spin box changes
     connect(spin_box_1, qOverload<int>(&QSpinBox::valueChanged),
             MyStore::instance().integer.setter());
 
     auto* spin_box_2 = new QSpinBox;
-    // Easier and flexible lambda subscription
-    MyStore::instance().integer.subscribe(
-        [spin_box_2](int value) { spin_box_2->setValue(value); });
+    // Easier and more flexible lambda subscription
+    MyStore::instance().integer.subscribe([spin_box_2](int value) {
+        spin_box_2->setValue(value);
+    });
     connect(spin_box_2, qOverload<int>(&QSpinBox::valueChanged),
             MyStore::instance().integer.setter());
 
-    // StoreString
-    auto* label = new QLabel;
-    MyStore::instance().string.subscribe(
-        std::bind(&QLabel::setText, label, _1));
 
+    // Store a string and using it with a line edit and a label
     auto* line_edit = new QLineEdit;
-    MyStore::instance().string.subscribe(
-        [line_edit](const QString& value) { line_edit->setText(value); });
+    MyStore::instance().string.subscribe([line_edit](const QString& value) {
+        line_edit->setText(value);
+    });
     connect(line_edit, &QLineEdit::textChanged,
             MyStore::instance().string.setter());
 
+    auto* label = new QLabel;
+    MyStore::instance().string.subscribe(
+        std::bind(&QLabel::setText, label, std::placeholders::_1));
+
+
+    // Setup and show the window (no connect!)
     auto* central_layout = new QVBoxLayout;
     central_layout->addWidget(spin_box_1);
     central_layout->addWidget(spin_box_2);
